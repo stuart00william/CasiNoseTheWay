f23a4d75f5022d1f913f2ab28913b346113a431a
Stuart H <stuart00william@gmail.com>
2015-02-11 13:16:24 -0500

    Update main.c
    added craps functionality

--- main.c
+++ main.c
@@ -2,8 +2,8 @@
 //  main.c
 //  CasiNoseTheWay
 //
-//  Created by Stuart Healey on 2/10/15.
-//  Copyright (c) 2015 Stuart Healey. All rights reserved.
+//  Created by Stuart H on 2/10/15.
+//  Copyright (c) 2015 Stuart H. All rights reserved.
 //
 
 #include <stdio.h>
@@ -13,6 +13,7 @@
 
 #include "player.h"
 #include "guessing_game.h"
+#include "craps.h"
 
 
 int main(int argc, const char * argv[]) {
@@ -21,24 +22,26 @@ int main(int argc, const char * argv[]) {
     PLAYER currentPlayer=createNewPlayer();
 
     //srand(time(NULL));
-    
+
     while (choice != 5) {
+        system("cls");
         printf("\n\n");
-        printf("-= Casino Knows The Way =-\n");
+        printf("-=        Casino        =-\n");
         printf("-*#^^^^^^^^^^^^^^^^^^^^#*-\n");
         printf("1 - Display Player Data\n");
         printf("2 - Reset Player\n");
         printf("3 - Guessing Game\n");
-        printf("4 - \n");
+        printf("4 - Craps Game\n");
         printf("5 - Exit\n\n");
 
         printf("[UserName: %s]\n",currentPlayer.userName);
         printf("[Chips   : %d]\n\n",currentPlayer.chips);
-        
+
         printf("Selection: ");
         scanf("%d",&choice);
+        system("cls");
         printf("\n\n");
-               
+
         if ((choice < 1) || (choice > 5)) {
             printf("Bad Selection");
         }
@@ -56,7 +59,8 @@ int main(int argc, const char * argv[]) {
                 currentPlayer.record[1]++;
         }
         else if (choice == 4) {
-            displayUserData(currentPlayer);
+            crapsWelcome();
+            crapsGame(currentPlayer);
         }
     }
     return 0;
