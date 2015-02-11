9e8915ec20a98dcf7aff4b01b6de47157c97521d
Stuart H <stuart00william@gmail.com>
2015-02-11 13:15:51 -0500

    Create craps.h

---
+++ craps.h
@@ -0,0 +1,21 @@
+//
+//  craps.h
+//  CasiNoseTheWay
+//
+//  Created by Stuart H on 2/10/15.
+//  Copyright (c) 2015 Stuart H. All rights reserved.
+//
+
+#ifndef __CasiNoseTheWay__craps__
+#define __CasiNoseTheWay__craps__
+
+
+#include <stdio.h>
+#include "player.h"
+
+
+void crapsWelcome(void);
+int crapsGame(PLAYER currentPlayer);
+
+
+#endif /* defined(__CasiNoseTheWay__player__) */
